6d75e2a179f2c43b5309d88ed3166b74411c8327
unknown <ogzgl06@gmail.com>
2021-11-03 17:14:56 +0300

    Configuration Template Engine with ejs

--- NodeJS-Week2/CleanBlogApp/app.js
+++ NodeJS-Week2/CleanBlogApp/app.js
@@ -5,12 +5,28 @@ const app = express();
 // Import path module
 const path = require('path');
 
+// Import ejs module
+const ejs = require('ejs');
+
+// Template Engine
+app.set('view engine', 'ejs');
+
 //Static file configuration - (Middleware)
 app.use(express.static('public'));
 
-// on the request to root page
+// on the request to root page - ROUTES
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+    res.render('index');
+});
+
+// on the request to about page - ROUTES
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+// on the request to post page - ROUTES
+app.get('/add_post', (req, res) => {
+    res.render('add_post');
 });
 
 // Port listening and configuration
